4d74209fdcd13dcd49fd0ff473b94d5cadea17f3
Guillermo Siles Bonilla <joker.crimson@gmail.com>
2015-11-03 14:16:49 +0100

    Actualización
    He probado antes de cambiar la función de void a analizadorDeDatos que el mapa y datos_atributos han guardado los datos correctamente. 
    
    Al final creo una clase analizadorDeDatos y la mando por return con el mapa y los atributos.

--- src/Lectura_Datos.java
+++ src/Lectura_Datos.java
@@ -5,42 +5,47 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Lectura_Datos{
-	public static void main(String [] args){
+	public static analizadorDeDatos crea_analizador(String ruta){
 		
-		// Este string podria darse como argumento de entrada.
-		String csvFile = "ejemplo1.csv";
+		// Aquí se da la ruta del archivo csv.
+		String csvFile = ruta;
 		
-		BufferedReader br = null;      			// Para leer el csv
-		String line = "";						// La variable que almacena la linea actual en cada iteración.
+		BufferedReader br = null;      					// Para leer el csv
+		String line = "";						// La variable que almacena la linea actual en cada iteracion.
 		String [] cabecera;						// La variable que almacena la primera linea.
-		String cvsSplitBy = ",";				// El token separador.
+		String cvsSplitBy = ",";					// El token separador.
 		Map<Integer,List<String>> mapa = new TreeMap<Integer,List<String>>();
 		String [][] matriz;						// La matriz con los datos.
-		int cont;								// El contador que nos indica la posición actual a rellenar.
+		int cont;							// El contador que nos indica la posicion actual a rellenar.
 		List<Atributo> datos_atributos = new ArrayList<Atributo>();
 		
-		// Vamos a leer una linea por cada iteración del bucle while. 
 		try{
 			br = new BufferedReader(new FileReader(csvFile));
+			line = br.readLine();
+		}catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
 			
-			// Primero leemos la primera linea y la almacenamos en cabecera.
-			cabecera = line.split(cvsSplitBy);
-			// En este for relleno una lista con todos los objetos atributos (todavía no tienen los valores añadidos).
-			for(int i = 0; i < cabecera.length; i++){
-				Atributo aux = new Atributo(cabecera[i]);
-				datos_atributos.add(aux);
-			}
-			
-			matriz = new String [100][cabecera.length];
-			cont = 0;						// Necesito este contador para saber si me he quedado sin espacio.
-			
-			// En este punto ya he terminado con el procesamiento de los nombres de los atributos. Empiezo con los datos.
+		// Primero leemos la primera linea y la almacenamos en cabecera.
+		cabecera = line.split(cvsSplitBy);
+		// En este for relleno una lista con todos los objetos atributos (todavía no tienen los valores añadidos).
+		for(int i = 0; i < cabecera.length; i++){
+			datos_atributos.add(new Atributo(cabecera[i]));
+		}
+		
+		matriz = new String [100][cabecera.length];
+		cont = 0;
+		
+		// En este punto ya he terminado con el procesamiento de los nombres de los atributos. Empiezo con los datos.		
+		try{
 			while ((line = br.readLine()) != null){
 				// Primero relleno una matriz con los datos.
 				if(cont >= matriz.length){					// Este if aumenta el tamaño de la matriz si se queda pequeña.
 					Arrays.copyOf(matriz, matriz.length + 100);
 				}
-				matriz[cont] = line.split(cvsSplitBy); 		// Aquí añado a la matriz la fila que toca.
+				matriz[cont] = line.split(cvsSplitBy); 				// Aqui añado a la matriz la fila que toca.
 				
 				// Ahora relleno el mapa y el set de atributos.
 				List<String> lista = new ArrayList<String>();
@@ -48,20 +53,29 @@ public class Lectura_Datos{
 					lista.add(matriz[cont][i]);
 					Atributo aux = datos_atributos.get(i);
 					aux.add(matriz[cont][i]);
-					// Con estas dos últimas lineas deberia quedar añadido el valor al objeto atributo.
+					// Con estas dos ultimas lineas deberia quedar añadido el valor al objeto atributo.
 				}
 				mapa.put(cont, lista);
 				
 				cont++;
 			}
 			br.close();
-		}catch (FileNotFoundException e) {
-			e.printStackTrace();
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
 		
-		/* La variable cont nos dice el número de paciente que tenemos. ¿Podría usar Arrays.copyof para descartar 
-	 	las lineas sobrantes? */
+		analizadorDeDatos date = new analizadorDeDatos(mapa,datos_atributos);
+		return date;
+		
+		// Aquí unos prints para comprobar si los datos se han leido bien.
+		/*
+		for(int i = 0; i < datos_atributos.size(); i++){
+			System.out.println(datos_atributos.get(i).toString());
+		}
+		System.out.println("\nPacientes.");
+		for(int i = 0; i < mapa.size(); i++){
+			System.out.println("Paciente " + i + ": " + mapa.get(i).toString());
+		}
+		*/
 	}
 }
