aa1b56096df4610bcd80eafe42ee375e4e7b7567
Kyaordean <kyaordean@gmail.com>
2015-11-29 23:29:59 +0100

    GUI
    Añadida interfaz simple y cambio de nombre en paquetes

---
+++ src/GUI/GUIArbol.java
@@ -0,0 +1,174 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import id3.Inicializador;
+ 
+public class GUIArbol extends JFrame {
+ 
+    private JPanel northPane;
+    private JPanel centerPane;  
+    private JPanel southPane;
+    private JTextField textField;
+    private JTextArea textArea;
+    private JLabel textLabel;
+    private JTextField colTextField;
+    private File archivoElegido;
+    private JLabel pathLabel;
+ 
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    GUIArbol frame = new GUIArbol();
+                    frame.pack();
+                    frame.setVisible(true);
+                    
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+ 
+    /**
+     * Create the frame.
+     */
+    public GUIArbol(){
+         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setPreferredSize(new Dimension(600, 400));
+        setLayout(new BorderLayout());
+        setTitle("ID3");
+      
+        /* NORTH Panel*/
+        
+        northPane = new JPanel();
+        northPane.setLayout(new GridLayout(3, 1));
+ 
+        textLabel = new JLabel("Seleccione el fichero de datos (CSV)");
+        northPane.add(new JPanel().add(textLabel));
+        
+        JPanel centerNorth = new JPanel();
+        centerNorth.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        textField = new JTextField();
+        textField.setToolTipText("Selecciona un fichero de datos");   
+        textField.setPreferredSize(new Dimension(250, 25));
+        
+        JButton btnSeleccionar = new JButton("Seleccionar");
+        
+        centerNorth.add(textField).setLocation(1, 0);
+        centerNorth.add(btnSeleccionar);
+        
+        northPane.add(centerNorth);
+        northPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        
+        JPanel southNorth = new JPanel();
+        
+        JLabel textLabelCol = new JLabel("Nº Columnas"); 
+        southNorth.add(textLabelCol);
+        southNorth.setLayout(new FlowLayout(FlowLayout.LEFT));
+        
+        colTextField = new JTextField();
+        colTextField.setToolTipText("Indicar número de columnas. Por defecto 0.");
+        colTextField.setPreferredSize(new Dimension(50, 25));
+        southNorth.add(colTextField);
+        
+        JButton run = new JButton("Iniciar");
+        southNorth.add(run);
+                    
+        northPane.add(southNorth).setLocation(2,0);
+        
+        add(northPane, BorderLayout.NORTH);
+        /*END North Panel*/
+        
+        /*Center Panel*/
+
+      textArea = new JTextArea();
+      textArea.setLineWrap(true);
+      textArea.setWrapStyleWord(true);
+   
+      Border border = BorderFactory.createLineBorder(Color.BLACK);
+      textArea.setBorder(BorderFactory.createCompoundBorder(border, 
+                  BorderFactory.createEmptyBorder(5, 20, 5, 20)));
+     ;
+      JScrollPane scroll=new JScrollPane(textArea);
+     
+      centerPane = new JPanel();
+      centerPane.setLayout(new BorderLayout());
+      centerPane.add(scroll);
+     
+      add(centerPane, BorderLayout.CENTER);
+      /*END Center Panel*/
+      
+      /*South Panel*/      
+      southPane = new JPanel();
+      
+      pathLabel = new JLabel();
+      southPane.add(pathLabel);
+      add(southPane, BorderLayout.SOUTH);
+  
+        btnSeleccionar.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+            	openSelector();
+            }
+        });
+        
+        
+        run.addActionListener(new ActionListener(){
+            public void actionPerformed (ActionEvent e){
+            	initID3();
+            pathLabel.setText(archivoElegido.getAbsolutePath());
+            }
+        });
+ 
+
+    }
+    
+    public void openSelector(){
+    	 JFileChooser fc = new JFileChooser();
+    
+    	  FileNameExtensionFilter filter = new FileNameExtensionFilter("Archivos CSV", "csv");
+          fc.setFileFilter(filter);
+         int respuesta = fc.showOpenDialog(this);
+         if (respuesta == JFileChooser.APPROVE_OPTION) {
+             archivoElegido = fc.getSelectedFile();
+             textField.setText(archivoElegido.getName());
+         
+         }
+
+    }
+    
+    public void initID3(){
+   	
+    	String ncolumnsString = colTextField.getText();
+    	int ncolumns = isNumeric(ncolumnsString) &&  Integer.parseInt(ncolumnsString)>=0 ?Integer.parseInt(ncolumnsString):0;
+   	
+    	Inicializador a1;
+    	a1 = new Inicializador(archivoElegido.getAbsolutePath(),ncolumns);
+    	textArea.setText(a1.procesar());
+
+    }
+    
+    public static boolean isNumeric(String str)
+    {
+      return str.matches("-?\\d+(\\.\\d+)?");  //match a number with optional '-' and decimal.
+    }
+    
+}
+
---
+++ src/id3/analizadorDeDatos.java
@@ -0,0 +1,134 @@
+package id3;
+
+import java.util.*;
+
+public class analizadorDeDatos {
+	Map<Integer,List<String>> datos;
+	List<Atributo> atributos;
+
+	public analizadorDeDatos(Map<Integer,List<String>> datos,List<Atributo> atributos){
+		this.datos=datos;
+		this.atributos=atributos;
+	}
+
+	/**
+	 * @param datos
+	 *            datos de entrada + atributos (True/False)
+	 * @param nodo
+	 *            nodo del arbol en el que estamos trabajando
+	 * @param hijo
+	 * 			  nºdel hijo si nodo no es vacio
+	 */
+	public Nodo procesarDatos(List<Boolean> filas,List<Boolean> columnas,String rama){
+		Nodo nodo;
+		//si entropia vale cero es un nodo hoja su atributo(todo si o no)
+		if(rama == " "){ //crear raiz y calcular ganancias
+			double gananciaMax =0;
+			int indice=0;
+			for(int i=1;i<this.getAtributos().size();i++){
+				System.out.println("Ganancia de "+this.getAtributos().get(i).nombre+" "+Func_Mat.ganancia(filas,this.getDatos(),i));
+				if( gananciaMax < Func_Mat.ganancia(filas,this.getDatos(),i)){
+					gananciaMax = Func_Mat.ganancia(filas,this.getDatos(),i);
+					indice=i;
+				}	
+			}
+			System.out.println("Nuevo nodo "+this.getAtributos().get(indice).toString());
+			nodo= new Nodo(this.getAtributos().get(indice).getNombre());
+			columnas.set(indice, false);
+			//System.out.println(this.getAtributos().get(indice).getValores());
+			for (String tipo : this.getAtributos().get(indice).getValores()){
+				//crear dos nuevas listas filas2 y columnas2 segun la rama y atributo actual
+				List<Boolean> filas2= procesarFilas(filas,tipo,indice);
+				//System.out.println("filas2"+filas2);
+				//System.out.println("Caso 1 por cada rama hacer "+tipo);
+				//System.out.println(Nodo.toStringTree(nodo));
+				nodo.anadirHijo(procesarDatos(filas2,columnas,tipo));
+			}
+			
+			
+
+		}else{ // calcular hijos
+			System.out.println("Caso 2  "+Func_Mat.valores(filas,this.getDatos(),0).size());
+			
+			if(Func_Mat.valores(filas,this.getDatos(),0).size()==1){ // si solo hay un tipo de atributo de salida, este sera el hijo
+				System.out.println("Caso 2.1");
+				//System.out.println(Func_Mat.valores(filas,this.getDatos(),0).keySet().toString());
+				System.out.println("Nuevo nodo "+Func_Mat.valores(filas,this.getDatos(),0).keySet().toString());
+				nodo= new Nodo(Func_Mat.valores(filas,this.getDatos(),0).keySet().toString(),rama);
+			}else{ //procesar datos para cada uno de los tipos de nodo
+				System.out.println("Caso 2.2");
+				//Caso 2.2.1 No hay filas
+				if(!hayFilas(filas)){
+					// Obtener el mayoritario
+					String nombre_nodo = Func_Mat.mayoritario(this.getDatos());
+
+					nodo = new Nodo(nombre_nodo, rama);
+				}else{
+					double gananciaMax =0;
+					int indice=0;
+					for(int i=1;i<this.getAtributos().size();i++){
+						if(columnas.get(i)){
+							System.out.println("Ganancia de "+this.getAtributos().get(i).nombre+" "+Func_Mat.ganancia(filas,this.getDatos(),i));
+							if(columnas.get(i) && gananciaMax < Func_Mat.ganancia(filas,this.getDatos(),i)){
+								gananciaMax = Func_Mat.ganancia(filas,this.getDatos(),i);
+								indice=i;
+							}	
+						}
+						
+					}
+					System.out.println("Nuevo nodo "+this.getAtributos().get(indice).toString());
+					nodo= new Nodo(this.getAtributos().get(indice).getNombre(),rama);
+					columnas.set(indice, false);
+					for (String tipo : this.getAtributos().get(indice).getValores()){
+						//crear dos nuevas listas filas2 y columnas2 segun la rama y atributo actual
+						//List<Boolean> filas2=filas,columnas2=columnas;//no terminado clonar y modificar
+						List<Boolean> filas2= procesarFilas(filas,tipo,indice);
+						nodo.anadirHijo(procesarDatos(filas2,columnas,tipo));
+					}
+				}
+				
+			}
+		}
+		return nodo;
+	}
+
+
+private boolean hayFilas(List<Boolean> filas) {
+		Boolean res=false;
+		Integer cont=0;
+		while(!res && cont <filas.size()){
+			res= filas.get(cont);
+			cont++;
+		}
+		return res;
+	}
+
+	//dado la columna y el tipo, debemos tachar los que no sean igual al tipo
+	private List<Boolean> procesarFilas(List<Boolean> filas, String tipo, int columna) {
+		List<Boolean> filas_new = new ArrayList<Boolean>();
+		filas_new.addAll(filas);
+		for(Map.Entry<Integer,List<String>> e: this.getDatos().entrySet()){
+			//System.out.println("for "+e.getKey()+" "+e.getValue().get(columna));
+			if(!e.getValue().get(columna).equalsIgnoreCase(tipo)){
+				filas_new.set(e.getKey(), false);
+			}
+		}
+		
+		return filas_new;
+	}
+
+	public Map<Integer, List<String>> getDatos() {
+		return datos;
+	}
+
+	public List<Atributo> getAtributos() {
+		return atributos;
+	}
+	
+	public String toString(){
+		return new String("Atributos: "+this.atributos.toString()+"\n Datos: \n"+this.getDatos().toString());
+	}
+
+
+
+}
